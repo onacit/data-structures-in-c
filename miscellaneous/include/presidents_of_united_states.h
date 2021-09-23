af5b70bbc2f5c2101f188236851e2668a12be12f
Jin Kwon <onacit@gmail.com>
2021-09-24 00:02:08 +0900

    wip

--- miscellaneous/include/presidents_of_united_states.h
+++ miscellaneous/include/presidents_of_united_states.h
@@ -3,8 +3,6 @@
 
 #define NUMBER_OF_PRESIDENTS_OF_UNITED_STATES 46
 
-#include <stdbool.h>
-
 struct president_of_united_state {
     unsigned char presidency;
     char president[23]; // William Henry Harrison (22)
